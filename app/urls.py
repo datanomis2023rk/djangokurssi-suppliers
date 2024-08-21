e619fafcfea4733920698758ef6832fd8d1d4c0b
datanomis2023rk <rami.korkeila@student.careeria.fi>
2024-08-21 14:36:51 +0300

    listaukset ja lisäykset toimii

--- app/urls.py
+++ app/urls.py
@@ -16,15 +16,16 @@ Including another URLconf
 """
 
 from django.urls import path
-from .views import landingview, productlistview, supplierlistview
+from .views import landingview, productlistview, supplierlistview, addsupplier, addproduct
 urlpatterns = [
     path('', landingview),
 
     # Products url´s
     path('products/', productlistview),
-
+    path('add-product/', addproduct),
     # Supplier url´s
-    path('suppliers/', supplierlistview)
-
+    path('suppliers/', supplierlistview),
+    path('add-supplier/', addsupplier),
+    
     
 ]
